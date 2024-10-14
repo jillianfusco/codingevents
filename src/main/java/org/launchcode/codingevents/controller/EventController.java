571fa11993d48199fd27efcb2a6979fa04ce71dd
Jillian Fusco <fuscojillian@gmail.com>
2024-10-14 09:07:42 -0400

    Form now adds events to page

--- src/main/java/org/launchcode/codingevents/controller/EventController.java
+++ src/main/java/org/launchcode/codingevents/controller/EventController.java
@@ -3,7 +3,9 @@ package org.launchcode.codingevents.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,15 +14,11 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
+    private static List<String> events = new ArrayList<>();
+
     @GetMapping
     public String displayEvents(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("JavaScript Workshop");
-        events.add("Spring Boot Seminar");
-        events.add("Python for Beginners");
-
         model.addAttribute("events", events);
-
         return "events/index";
     }
 
@@ -28,5 +26,12 @@ public class EventController {
     @GetMapping("create")
     public String renderCreateEventForm() {
         return "events/create";
+    }
+
+    //lives at /events/create
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName) {
+        events.add(eventName);
+        return "redirect:/events";
     }
 }
